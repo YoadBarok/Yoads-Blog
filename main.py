bc67993f69d15f8a708dcf64e9ae0da91ec08e6c
YoadBarok <YoadBarok@users.noreply.github.com>
2021-07-16 00:45:10 +0200

    Make contact form work

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ def add_new_post():
                            current_user=current_user)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @admin_only
 def edit_post(post_id):
     if not current_user.is_authenticated:
@@ -233,7 +233,6 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
